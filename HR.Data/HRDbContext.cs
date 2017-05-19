ee112b5de60147aaee744e8a4854d9b99a1d6cdb
Meena <meena@logicon.com>
2017-05-19 11:28:08 +0530

    my first push

---
+++ HR.Data/HRDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace HR.Data
+{
+    public class HRDbContext:DbContext
+    {
+        public HRDbContext() : base("name=HRContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
